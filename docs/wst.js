480945d66917197f2000b3a78a720805740d6c05
greendatainfo <104235111+greendatainfo@users.noreply.github.com>
2022-04-24 15:43:26 -0400

    Update wst.js

--- docs/wst.js
+++ docs/wst.js
@@ -16,12 +16,64 @@
   <button onclick="document.location='wst.js'">wst</button>
   <p>Welcome to GreenDataInfo!  We're dedicated to providing the public with environmental conservation information from the public sectior in an easy-to-read format.</p>
   <div>
-from requests_html import HTMLSession
-s = HTMLSession()
-r = s.get('https://metro.co.uk/news/tech/')
-posts = r.html.find('ul.metro-mosaic', first=True).find('h3')
-for post in posts:
-print(post.text)
+import * as fetch from "node-fetch";
+import * as cheerio from "cheerio";
+import * as fs from "fs";
+import * as path from "path";
+import * as urlParser from "url";
+
+const seenUrls = {};
+
+const getUrl = (link, host, protocol) => {
+  if (link.includes("http")) {
+    return link;
+  } else if (link.startsWith("/")) {
+    return `${protocol}//${host}${link}`;
+  } else {
+    return `${protocol}//${host}/${link}`;
+  }
+};
+
+const crawl = async ({ url, ignore }) => {
+  if (seenUrls[url]) return;
+  console.log("crawling", url);
+  seenUrls[url] = true;
+
+  const { host, protocol } = urlParser.parse(url);
+
+  const response = await fetch(url);
+  const html = await response.text();
+  const $ = cheerio.load(html);
+  const links = $("a")
+    .map((i, link) => link.attribs.href)
+    .get();
+
+  const imageUrls = $("img")
+    .map((i, link) => link.attribs.src)
+    .get();
+
+  imageUrls.forEach((imageUrl) => {
+    fetch(getUrl(imageUrl, host, protocol)).then((response) => {
+      const filename = path.basename(imageUrl);
+      const dest = fs.createWriteStream(`images/${filename}`);
+      response.body.pipe(dest);
+    });
+  });
+
+  links
+    .filter((link) => link.includes(host) && !link.includes(ignore))
+    .forEach((link) => {
+      crawl({
+        url: getUrl(link, host, protocol),
+        ignore,
+      });
+    });
+};
+
+crawl({
+  url: "http://stevescooking.blogspot.com/",
+  ignore: "/search",
+});
   </div>
 </body>
   <footer>
